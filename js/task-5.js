4fd86cdbde082f5b5c536c25cd5d3a4a592cb6fb
IrinaPapulova <142151842+IrinaPapulova@users.noreply.github.com>
2024-01-19 12:43:10 +0200

    task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,18 +1,17 @@
 const button = document.querySelector(".change-color");
 const body = document.querySelector("body");
-
-const pText = document.querySelector("p");
 const spanColor = document.querySelector(".color");
+const changeColor = getRandomHexColor();
 
 button.addEventListener("click", changeBGC);
 
 function changeBGC() {
-  body.style.backgroundColor = getRandomHexColor();
-  spanColor.textContent = getRandomHexColor();
-}
+  body.style.backgroundColor = changeColor;
+  spanColor.textContent = changeColor;
+};
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, '0')}`;
-}
+};
